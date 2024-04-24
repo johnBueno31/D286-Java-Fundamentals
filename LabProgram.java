d65e141b41679f54c6eaab730b05a4ddb75579a3
John <johnranierbueno@gmail.com>
2024-04-24 16:16:52 -0700

    lab 1

---
+++ LabProgram.java
@@ -0,0 +1,10 @@
+public class LabProgram {
+    public static void main(String[] args) {
+
+        System.out.println("H   H");
+        System.out.println("H   H");
+        System.out.println("HHHHH");
+        System.out.println("H   H");
+        System.out.println("H   H");
+    }
+}
